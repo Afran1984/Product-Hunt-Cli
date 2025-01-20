5d03e89bdae9a1c2c4cda2e40a61ccd7ca9bf5fd
Afran1984 <afranrazzak1984@gmail.com>
2025-01-20 16:31:31 +0600

    update

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/shared/Navbar";
+import FooterHunt from "../components/shared/FooterHunt";
+
 
 const Main = () => {
   return <div className="max-w-[1240px] mx-auto px-4">
         <Navbar/>
         <Outlet/>
+        <FooterHunt/>
   </div>;
 };
 
