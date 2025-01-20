740ed605a441c31636b676c40571df6ac5feda19
Akther Hosen <dev.aktherhosen@gmail.com>
2025-01-20 11:50:06 +0600

    added navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Main from './layout/Main'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+        <Route path="/" element={<Main />}>
+        <Route index="true" element={<Home/>}></Route>
+        </Route>
     </Routes>
   </BrowserRouter>
   </StrictMode>,
